d13b9b220c1fc9e09d57ec4aa2fbf42bf1245292
Aleksandar Stevkovski <astevkovski87@gmail.com>
2025-04-12 21:54:44 +0200

    updated

--- backend/pkg/db/db.js
+++ backend/pkg/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-const uri = process.env.URI;
+const [MONGO_USER, MONGO_PASS, MONGO_NAME] = process.env;
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.frwhldk.mongodb.net/${MONGO_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 
 const connectDB = async () => {
   try {
