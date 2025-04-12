05262f0631fc55411c834cdf06dab94219c480dd
Aleksandar Stevkovski <astevkovski87@gmail.com>
2025-04-12 20:19:53 +0200

    vercel update

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -28,7 +28,7 @@ const SignUpPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch(`${process.env.FETCH_URL}`, {
+      const res = await fetch("/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
